e31b8eb40fee14c7b1c5d8f0b31e5804ddc77d3e
Brian Faust <faustbrian@users.noreply.github.com>
2017-06-05 13:14:44 +0300

    Apply fixes from StyleCI (#25)
    [ci skip] [skip ci]

--- src/Leaderboard/LeaderboardRepository.php
+++ src/Leaderboard/LeaderboardRepository.php
@@ -27,7 +27,7 @@ class LeaderboardRepository
 
         // data for create and update
         $data = [
-            'boardable_id' => $model->id,
+            'boardable_id'   => $model->id,
             'boardable_type' => get_class($model),
             // 'position' => $position,
             'experience' => $experience,
